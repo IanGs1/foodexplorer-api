65eaef42e70ab8635272e77bb7e5125614571662
Ian <goldfeld35@gmail.com>
2024-01-22 18:20:40 -0300

    AppError Class & lib: express-async-errors ❌

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
+require("express-async-errors");
 const express = require("express");
+
+const AppError = require("./utils/AppError");
 
 const app = express();
 
 app.use(express.json());
+
+app.use((error, request, reply, next) => {
+  if (error instanceof AppError) {
+    return reply.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    })
+  }
+
+  return reply.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  })
+})
 
 app.listen(3333, () => {
   console.log("HTTP server listening on: http://localhost:3333 🚀");
