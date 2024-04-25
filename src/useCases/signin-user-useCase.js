0c3ca261e6df43e41f5fe1a7aa0e413acee29488
IanGs1 <goldfeld35@gmail.com>
2024-04-25 11:43:32 -0300

    feat: sign in user service

---
+++ src/useCases/signin-user-useCase.js
@@ -0,0 +1,33 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
+const jwtConfig = require("../config/jwt-config");
+
+const { sign } = require("jsonwebtoken");
+const { compare } = require("bcryptjs");
+
+class SignInUserUseCase {
+  async execute({ email, password }) {
+    const user = await knex("users").where({ email }).first();
+    if (!user) {
+      throw new AppError("Email/Password is invalid!", 400);
+    };
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new AppError("Email/Password is invalid!", 400);
+    };
+
+    const token = sign({}, jwtConfig.secret, {
+      subject: user.id,
+      expiresIn: jwtConfig.expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  };
+};
+
+module.exports = SignInUserUseCase;
