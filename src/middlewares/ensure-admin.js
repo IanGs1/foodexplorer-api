f4290c3730c033f131dd76f1167d0796acf8fbc3
IanGs1 <goldfeld35@gmail.com>
2024-04-26 11:13:29 -0300

    feat: ensure admin middleware

---
+++ src/middlewares/ensure-admin.js
@@ -0,0 +1,12 @@
+function ensureAdmin(request, reply, next) {
+  const { role: userRole } = request.user;
+
+  if (userRole !== "admin") {
+    return reply.status(401).send({
+      status: "error",
+      message: "User has no permission to access this route!",
+    });
+  };
+
+  return next();
+};
