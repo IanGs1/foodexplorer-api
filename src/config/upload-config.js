f40561452bb6354eaf247d0313f1dfed5dcfcf02
IanGs1 <goldfeld35@gmail.com>
2024-04-28 13:42:03 -0300

    config: creating the uploads config file

---
+++ src/config/upload-config.js
@@ -0,0 +1,26 @@
+const multer = require("multer");
+
+const path = require("node:path");
+const crypto = require("node:crypto");
+
+const tmpFolder = path.resolve("..", "..", "tmp");
+
+const uploadsFolder = path.resolve(tmpFolder, "uploads");
+
+const multerConfig = {
+  storage: multer.diskStorage({
+    destination: tmpFolder,
+    filename: (_, file, callback) => {
+      const fileHash = crypto.randomBytes(7).toString("hex");
+      const fileName = `${fileHash}-${file.originalname}`;
+
+      return callback(null, fileName);
+    },
+  }),
+};
+
+module.exports = {
+  tmpFolder,
+  uploadsFolder,
+  multerConfig,
+};
